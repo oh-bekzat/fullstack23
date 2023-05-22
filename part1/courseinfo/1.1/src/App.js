ec1637226fc28bebc52abc30fa50e1f01e92f240
oh-bekzat <111849517+oh-bekzat@users.noreply.github.com>
2023-05-23 00:39:36 +0600

    worked a bit on 1d, finished up to1.5

--- part1/courseinfo/1.1/src/App.js
+++ part1/courseinfo/1.1/src/App.js
@@ -1,46 +1,18 @@
+import {useState} from 'react'
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  return (
-    <div>
-      <Header course = {course} />
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
-      <Total total = {exercises1 + exercises2 + exercises3} />
-    </div>
-  )
-}
+  const [value, setValue] = useState(10)
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>
-        {props.course}
-      </h1>
-    </div>
-  )
-}
- 
-const Content = (props) => {
-  return (
-    <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
-    </div>
-  )
-}
+  const hello = (who) => () => {
+      console.log('hello', who)
+  }
 
-const Total = (props) => {
   return (
     <div>
-      <p>
-        Number of exercises {props.total}
-      </p>
+      {value}
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
     </div>
   )
 }
