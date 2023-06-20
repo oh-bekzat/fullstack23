78b97a1dcf17d29fafb95538fabcfa6d64a327e3
oh-bekzat <111849517+oh-bekzat@users.noreply.github.com>
2023-06-20 19:25:26 +0600

    part 4 done
    The whole part is here, some old code that I shouldve committed is commented. See older versions named by task number if anything important has changed

---
+++ part2/phonebook_frontend/src/services/persons.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const baseUrl = '/api/persons'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const remove = id => {
+  const url = `${baseUrl}/${id}`
+  const request = axios.delete(url)
+  return request.then(response => response.data)
+}
+
+const update = (id, updatedObject) => {
+  const url = `${baseUrl}/${id}`
+  const request = axios.put(url, updatedObject)
+  return request.then(response => response.data)
+}
+
+const personService = {
+  getAll,
+  create,
+  remove,
+  update
+}
+
+export default personService
