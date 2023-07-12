606c3914a450e33acf2c6564aad8ff534208ff59
oh-bekzat <111849517+oh-bekzat@users.noreply.github.com>
2023-07-13 01:09:11 +0600

    6.12

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
 import Filter from './components/Filter'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Notification />
       <Filter />
       <AnecdoteList />
       <AnecdoteForm />
