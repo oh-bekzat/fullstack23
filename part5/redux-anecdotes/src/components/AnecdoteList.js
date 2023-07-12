8f89b3c75e34fed3d246905a76f983c3eea15cbb
oh-bekzat <111849517+oh-bekzat@users.noreply.github.com>
2023-07-12 23:08:02 +0600

    6.9

--- part5/redux-anecdotes/src/components/AnecdoteList.js
+++ part5/redux-anecdotes/src/components/AnecdoteList.js
@@ -3,23 +3,26 @@ import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
 
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => state.anecdotes)
+    const filter = useSelector(state => state.filter)
     const dispatch = useDispatch()
   
     const vote = (id) => {
         dispatch(voteAnecdote(id))
     }
+
+    const filteredAnecdotes = anecdotes.filter((anecdote) =>
+        anecdote.content.toLowerCase().includes(filter.toLowerCase())
+    )
     
     return (
         <>
-            {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+            {filteredAnecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
             <div key={anecdote.id}>
+                <div>{anecdote.content}</div>
                 <div>
-                {anecdote.content}
-                </div>
-                <div>
-                has {anecdote.votes}
-                <button onClick={() => vote(anecdote.id)}>vote</button>
+                    has {anecdote.votes}
+                    <button onClick={() => vote(anecdote.id)}>vote</button>
                 </div>
             </div>
             )} 
--- redux-notes/src/reducers/noteReducer.js
+++ redux-notes/src/reducers/noteReducer.js
@@ -1,41 +1,54 @@
-const noteReducer = (state = [], action) => {
-    switch(action.type) {
-      case 'NEW_NOTE':
-        return [...state, action.payload]
-      case 'TOGGLE_IMPORTANCE':
-        const id = action.payload.id
-        const noteToChange = state.find(n => n.id === id)
-        const changedNote = { 
-          ...noteToChange, 
-          important: !noteToChange.important 
-        }
-        return state.map(note =>
-          note.id !== id ? note : changedNote 
-        )
-      default:
-        return state
+const initialState = [
+  {
+    content: 'reducer defines how redux store works',
+    important: true,
+    id: 1,
+  },
+  {
+    content: 'state of store can contain any data',
+    important: false,
+    id: 2,
+  },
+]
+
+const noteReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'NEW_NOTE':
+      return [...state, action.payload]
+    case 'TOGGLE_IMPORTANCE':
+      const id = action.payload.id
+      const noteToChange = state.find(n => n.id === id)
+      const changedNote = { 
+        ...noteToChange, 
+        important: !noteToChange.important 
       }
-    } 
-  
-  const generateId = () =>
-    Number((Math.random() * 1000000).toFixed(0))
-  
-  export const createNote = (content) => {
-    return {
-      type: 'NEW_NOTE',
-      payload: {
-        content,
-        important: false,
-        id: generateId()
-      }
+      return state.map(note =>
+        note.id !== id ? note : changedNote 
+      )
+    default:
+      return state
+    }
+  } 
+
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
     }
   }
-  
-  export const toggleImportanceOf = (id) => {
-    return {
-      type: 'TOGGLE_IMPORTANCE',
-      payload: { id }
-    }
-  }  
-  
-  export default noteReducer
+}
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}  
+
+export default noteReducer
