cc09d8fae74297929f58f5f44623252a97a5a43f
oh-bekzat <111849517+oh-bekzat@users.noreply.github.com>
2023-07-13 12:28:06 +0600

    6.14, 6.15

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
+anecdoteService.getAll().then(notes =>
+  store.dispatch(setAnecdotes(notes))
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- redux-notes/src/index.js
+++ redux-notes/src/index.js
@@ -5,8 +5,9 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
+import noteService from './services/notes'
 
 const store = configureStore({
   reducer: {
@@ -15,7 +16,9 @@ const store = configureStore({
   }
 })
 
-console.log(store.getState())
+noteService.getAll().then(notes =>
+  store.dispatch(setNotes(notes))
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
